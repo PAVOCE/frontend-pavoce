feb7342659aa2c8591d132cf412da6c1ef4dda22
Nsama david <nsamadavidazaah@gmail.com>
2023-07-22 00:20:55 +0100

    setup: seting up context, layout, lazy loading, routing, tailwindcss, angular material, platform browser init, jasmine testing

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import {SharedModule} from "../../../modules/shared.module";
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    DashboardRoutingModule
+  ]
+})
+export class DashboardModule{
+}
